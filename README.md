0a95ab4ad32bee779a45b6a2d402efbdaa204050
Matteo De Donno <matteodedonno02@gmail.com>
2023-05-10 08:50:25 +0200

    First and last commit of this stupid project

---
+++ README.md
@@ -0,0 +1,34 @@
+# LOLlu
+<p align="center">
+    <img src="assets/LOLlu.jpg"  width="100">
+</p>
+
+
+Funny Telegram Bot that write in a personal or group chat every time you or your friends playing League Of Legends!
+
+## Usage
+
+- ```
+  git clone https://github.com/matteodedonno02/LOLlu.git
+  ```
+- ```
+  cp config.py.example config.py
+  ```
+
+- Replace **players** with an existing players list.
+
+- Replace **RIOT_KEY** with your riot api key.
+
+- Replace **BOT_KEY** with your telegram bot key and **CHAT_ID** with your telegram chat id or telegram group chat id
+
+ - Run command
+
+    ```
+    docker-compose up
+    ```
+
+    that build image and run container
+
+## Is it necessary for such a simple little program to use docker?
+
+No but dockerizing is so funny :whale2: :whale2:
---
+++ main.py
@@ -0,0 +1,50 @@
+from config import ENDPOINT
+from config import RIOT_KEY
+from config import ENDPOINT2
+from config import BOT_KEY
+from config import CHAT_ID
+from config import players
+import requests
+import telegram
+import asyncio
+import time
+
+def main():
+    players_ids = get_player_id_map(players)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    tasks = []
+    for player, id in players_ids.items():
+        tasks.append(loop.create_task(send_msg(player, id)))
+    loop.run_until_complete(asyncio.gather(*tasks))
+
+async def send_msg(name, id):
+    bot = telegram.Bot(BOT_KEY)
+    is_playing = False
+    while(True):
+        if fetch_api(id):
+            if not is_playing:
+                await bot.send_message(CHAT_ID, "Che lollacciu! " + name + " sta scioca a LOL!")
+                is_playing = True
+        else:
+            is_playing = False
+        time.sleep(60)
+
+def get_player_id_map(players):
+    map = {}
+    for player in players:
+        response = requests.get(ENDPOINT2 + player + "?api_key=" + RIOT_KEY)
+        data = response.json()
+        map[player] = data["id"]
+    return map
+
+def fetch_api(id):
+    response = requests.get(ENDPOINT + id + "?api_key=" + RIOT_KEY)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    main()
